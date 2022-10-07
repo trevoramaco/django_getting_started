6e0121a57b4ac23b748d6231178586e363b4d148
Trevor Maco <tmaco@cisco.com>
2022-10-07 15:02:04 -0400

    modelforms and submitting data

--- meeting_planner/meetings/urls.py
+++ meeting_planner/meetings/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('rooms', views.rooms, name="rooms"),
-    path('<int:id>', views.detail, name='detail')
+    path('rooms', views.rooms, name='rooms'),
+    path('<int:id>', views.detail, name='detail'),
+    path('new', views.new, name='new')
 ]
--- meeting_planner/meetings/views.py
+++ meeting_planner/meetings/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Meeting, Room
+
+from django.forms import modelform_factory
+from .forms import MeetingForm
 
 
 # Create your views here.
@@ -11,5 +14,18 @@ def detail(request, id):
 def rooms(request):
     return render(request, "meetings/rooms.html", {"rooms": Room.objects.all()})
 
+
+# Note: MeetingForm is a class! Not a regular object...
+# MeetingForm = modelform_factory(Meeting, exclude=[])
+
 def new(request):
-    return render(request, "meetings/new.html")
+    if request.method == "POST":
+        # Form has been submitted, process data
+        form = MeetingForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("welcome")
+    else:
+        form = MeetingForm()
+
+    return render(request, "meetings/new.html", {"form": form})
